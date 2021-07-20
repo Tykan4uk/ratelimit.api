bf36a3760785eb4c1b137a8b8783ba10fa528c82
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 12:04:17 +0300

    ManageBffController: renaming to RateLimitBffController. methods now get request.

--- RateLimit/src/RateLimit/Controllers/RateLimitBffController.cs
+++ RateLimit/src/RateLimit/Controllers/RateLimitBffController.cs
@@ -1,36 +1,32 @@
 ﻿using System.Threading.Tasks;
-using RateLimitApi.Configuration;
 using RateLimitApi.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using RateLimitApi.Models.Responses;
+using RateLimitApi.Models.Requests;
 
 namespace RateLimitApi.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]/[action]")]
-    public class ManageBffController : ControllerBase
+    public class RateLimitBffController : ControllerBase
     {
         private readonly ILogger<ManageController> _logger;
         private readonly IRateLimitService _rateLimitService;
-        private readonly Config _config;
 
-        public ManageBffController(
+        public RateLimitBffController(
             ILogger<ManageController> logger,
-            IOptions<Config> config,
             IRateLimitService rateLimitService)
         {
             _logger = logger;
             _rateLimitService = rateLimitService;
-            _config = config.Value;
         }
 
         [HttpPost]
-        public async Task<CheckRateLimitResponse> CheckRateLimitAsync(string name)
+        public async Task<CheckRateLimitResponse> CheckRateLimitAsync([FromBody] CheckRateLimitRequest request)
         {
             Request.Headers.TryGetValue("Origin", out var requestOrigin);
-            var result = await _rateLimitService.CheckAsync($"{name}{requestOrigin}");
+            var result = await _rateLimitService.CheckAsync($"{request.Name}{requestOrigin}");
             return result;
         }
     }
