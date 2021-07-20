3db08866e1af4c9cf3f9bc1db4a219ed2384cef9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 12:03:32 +0300

    ManageController: now get request.

--- RateLimit/src/RateLimit/Controllers/ManageController.cs
+++ RateLimit/src/RateLimit/Controllers/ManageController.cs
@@ -1,10 +1,9 @@
 using System.Threading.Tasks;
-using RateLimitApi.Configuration;
 using RateLimitApi.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using RateLimitApi.Models.Responses;
+using RateLimitApi.Models.Requests;
 
 namespace RateLimitApi.Controllers
 {
@@ -23,11 +22,11 @@ namespace RateLimitApi.Controllers
             _rateLimitService = rateLimitService;
         }
 
-        [HttpPost]
-        public async Task<CheckRateLimitResponse> CheckRateLimitAsync(string name)
+        [HttpGet]
+        public async Task<CheckRateLimitResponse> CheckRateLimitAsync(CheckRateLimitRequest request)
         {
             var origin = Request.Headers.TryGetValue("Origin", out var requestOrigin);
-            var result = await _rateLimitService.CheckAsync($"{name}{origin}");
+            var result = await _rateLimitService.CheckAsync($"{request.Name}{origin}");
             return result;
         }
     }
